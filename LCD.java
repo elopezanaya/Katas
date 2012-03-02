c72df98b26d18d85ba3a9be9ef72fd5d84b57922
@iamcoder <elopezanaya@gmail.com>
2012-03-01 20:58:33 -0600

    solucion para mas de un numero

--- LCD.java
+++ LCD.java
@@ -1,54 +1,53 @@
 package katas.tdd.test;
 
+import java.io.IOException;
+
 public class LCD {
 
-	public String getLCDNumberRepresentation(int number) {
+	final String ENTER = "\n";
+	final String MODE1 = " - ";
+	final String MODE2 = "| |";
+	final String MODE3 = " - ";
+	final String MODE4 = "| |";
 
-		String representationOfTheNumberOnLCDWay="";
-		
-		final String ENTER="\n";
-		final String MODE1=  " - " ;
-		final String MODE2 = "| |" ;
-		final String MODE3 = " - " ;
-		final String MODE4 = "| |" ;
-		
-		final String MODE6=  "|  ";
-		final String MODE7 ="  |" ;
-		final String MODE8 ="   " ;
-		
-		switch (number){
-		
-		case 0 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE2 + ENTER + MODE8 + ENTER + MODE2 +ENTER+ MODE1;}
-		break;
-		case 1 :{ representationOfTheNumberOnLCDWay = MODE8 + ENTER + MODE7 + ENTER + MODE8 + ENTER + MODE7 +ENTER+ MODE8;}
-		break;
-		case 2 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE7 + ENTER + MODE3 + ENTER + MODE6 +ENTER+ MODE1;}
-				break;
-		case 3 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE7 + ENTER + MODE1 + ENTER + MODE7 +ENTER+ MODE1;}
-		break;
-		case 4 :{ representationOfTheNumberOnLCDWay = MODE2 + ENTER + MODE3 + ENTER + MODE7 ;}
-		break;
-		case 5 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE6 + ENTER + MODE1 + ENTER + MODE7 +ENTER+ MODE1;}
-		break;
-		case 6 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE6 + ENTER+ MODE1 +ENTER + MODE4 + ENTER + MODE1 ;}
-		break;
-		case 7 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE7 + ENTER + MODE8 + ENTER + MODE7 +ENTER+ MODE8;}
-		break;
-		case 8 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE2 + ENTER + MODE1 + ENTER + MODE2 +ENTER+ MODE1;}
-		break;
-		case 9 :{ representationOfTheNumberOnLCDWay = MODE1 + ENTER + MODE2 + ENTER + MODE1 + ENTER + MODE7 +ENTER+ MODE1;}
-		break;
+	final String MODE6 = "|  ";
+	final String MODE7 = "  |";
+	final String MODE8 = "   ";
+
+	final String howTheNumberisFormed[][] = {
+			{ MODE1, MODE2, MODE8, MODE2, MODE1 },// 0
+			{ MODE8, MODE7, MODE8, MODE7, MODE8 },// 1
+			{ MODE1, MODE7, MODE3, MODE6, MODE1 },// 2
+			{ MODE1, MODE7, MODE1, MODE7, MODE1 },// 3
+			{ MODE2, MODE3, MODE7 },// 4
+			{ MODE1, MODE6, MODE1, MODE7, MODE1 },// 5
+			{ MODE1, MODE6, MODE1, MODE4, MODE1 },// 6
+			{ MODE1, MODE7, MODE8, MODE7, MODE8 },// 7
+			{ MODE1, MODE2, MODE1, MODE2, MODE1 },// 8
+			{ MODE1, MODE2, MODE1, MODE7, MODE1 } // 9
+
+	};
+	final String SPACE =" " ;
+	public String getLCDNumberRepresentation(Integer number) throws NumberFormatException, IOException {
+
+		String representationOfTheNumberOnLCDWay = "";
+		char[] numbers = number.toString().toCharArray();
+		for (int i = 1; i <= 5; i++) {
+
+			for (char character : numbers) {
+				
+				representationOfTheNumberOnLCDWay += howTheNumberisFormed[Integer.parseInt(String.valueOf(character))][(i-1)]
+						+ SPACE;
+			}
+
+			representationOfTheNumberOnLCDWay += ENTER;
 		}
 
 		System.out.println("REPRESENTADO :");
 		System.out.println(representationOfTheNumberOnLCDWay);
-		
+
 		return representationOfTheNumberOnLCDWay;
-		
-		
-		
+
 	}
-
-
 
 }
